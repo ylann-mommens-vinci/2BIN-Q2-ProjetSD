a0167a0be07114afe3808ba58906af737d77f5f8
Asox7 <mommensylann@gmail.com>
2024-02-26 23:21:48 +0100

    feat: ajout de la liste d'adjacence

--- src/Graph.java
+++ src/Graph.java
@@ -1,13 +1,15 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Graph {
 
   private Map<String, City> idToCity = new HashMap<>();
+  private  Map<City, Set<Road>> outputRoad = new HashMap<>();
 
   public Graph(File cities, File roads) {
     try(Scanner sc = new Scanner(cities)) {
@@ -19,8 +21,36 @@ public class Graph {
       e.printStackTrace();
     }
 
+    try(Scanner scanner=new Scanner(roads)){
+      // On parcourt le fichier roads
+      while (scanner.hasNextLine()){
+        String[] line = scanner.nextLine().split(",");
+        // On récupère les villes de départ
+        City source = idToCity.get(line[0]);
+        City destination = idToCity.get(line[1]);
 
+        //On crée une nouvelle route
+        Road road = new Road(source, destination);
 
+        //Si ville est déja dans la map
+        if (outputRoad.containsKey(source)) {
+          //Si la route n'est pas déja dans la map
+          Set<Road> roadSet = outputRoad.get(source);
+          if(!roadSet.contains(road)) {
+            //On ajoute la route à la ville
+            roadSet.add(road);
+            outputRoad.put(source, roadSet);
+          }
+        }else {
+          //On ajoute la ville dans la map & On ajoute la route à la ville
+          Set<Road> roadSet = new HashSet<>();
+          roadSet.add(road);
+          outputRoad.put(source, roadSet);
+        }
+      }
+    }catch (FileNotFoundException e){
+      e.printStackTrace();
+    }
   }
 
   /* Algorithme BFS
