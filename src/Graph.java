643a2722983ca566936858a6a25376637a016d70
Maxime <maxime.issa@student.vinci.be>
2024-02-26 15:13:39 +0100

    Javadoc

--- src/Graph.java
+++ src/Graph.java
@@ -10,11 +10,20 @@ public class Graph {
     this.roads=roads;
   }
 
-  void calculerItineraireMinimisantNombreRoutes(String a, String b){
+  /* Algorithme BFS
+  Calcul l'itinéraire entre deux villes avec le moins de routes possibles
+  S'il est impossible d'aller d'une ville à l'autre -> Exception
+  */
+
+  void calculerItineraireMinimisantNombreRoutes(String v1, String v2){
 
   }
 
-  void calculerItineraireMinimisantKm(String a, String b){
+  /* Algorithme de Dijkstra
+  Calcul l'itinéraire entre deux villes avec le moins de kilomètres
+  S'il est impossible d'aller d'une ville à l'autre -> Exception
+  */
+  void calculerItineraireMinimisantKm(String v1, String v2){
 
   }
 
