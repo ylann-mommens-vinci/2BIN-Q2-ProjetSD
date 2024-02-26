74b585c0b0dc9e05b1663b1a02856d3cfdb753e1
Maxime <maxime.issa@student.vinci.be>
2024-02-26 14:30:09 +0100

    Graph

---
+++ src/Graph.java
@@ -0,0 +1,22 @@
+import java.io.File;
+
+public class Graph {
+
+  File cities;
+  File roads;
+
+  public Graph(File cities, File roads) {
+    this.cities=cities;
+    this.roads=roads;
+  }
+
+  void calculerItineraireMinimisantNombreRoutes(String a, String b){
+
+  }
+
+  void calculerItineraireMinimisantKm(String a, String b){
+
+  }
+
+
+}
